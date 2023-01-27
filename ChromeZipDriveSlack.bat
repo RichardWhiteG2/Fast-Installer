55f239d9fbc00c10495a9817425d3d62f74407c8
Brayan Ricardo Blanco García <102545085+RichardWhiteG2@users.noreply.github.com>
2023-01-26 20:17:39 -0600

    Se agrega opcion regresar en agente.bat

--- ChromeZipDriveSlack.bat
+++
@@ -1,4 +0,0 @@
-winget install --id=Google.Chrome -e  
-&& winget install --id=Google.Drive -e  
-&& winget install --id=7zip.7zip -e 
-&& winget install --id=SlackTechnologies.Slack -e
--- agente.bat
+++ agente.bat
@@ -5,18 +5,36 @@ echo Que ubicacion tiene destinada el equipo de computo:
 echo.
 echo 1. Corporativo
 echo 2. Foraneo
+echo 3. Regresar
 set /p var=
-if %var%==1 (
+if %var%==1 goto:Corporativo
+if %var%==2 goto:Foraneo
+if %var%==3 goto:Regresar
+:Corporativo
 echo.
 Echo Se comienza a instalar Agente CORPORATIVO ...
 pause
 start /d "C:\Fast-Installer" agenteCorporativo.exe
-)else (
+echo.
+Echo Compruebe que la instlacion de Agente CORPORATIVO finalizo...
+pause
+echo.
+Echo .:: *** La instalacion COMPLETA ha terminado *** ::.
+pause
+call CONFIGURACION.bat
+:Foraneo
 echo.
 Echo Se comienza a instalar Agente FORANEO ...
 pause
 start /d "C:\Fast-Installer" agenteForaneo.exe
-)
+echo.
+Echo Compruebe que la instlacion de Agente FORANEO finalizo...
 pause
+echo.
+Echo .:: *** La instalacion COMPLETA ha terminado *** ::.
+pause
+call CONFIGURACION.bat
+:Regresar
+call CONFIGURACION.bat
 
 
